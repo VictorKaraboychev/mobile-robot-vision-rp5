f1f8b3bf1cd09fade25be643fc9b54692bffd7b9
Sidharth Sreeram <sidharthsreeram@gmail.com>
2025-03-23 22:09:29 -0400

    height

--- src/velocityVector.py
+++ src/velocityVector.py
@@ -174,7 +174,7 @@ def main():
             dx, dy = trajectory
             gpx, gpy = find_real_world_coordinates(dx, dy)
 
-            dist_x, dist_y = (gpx, gpy)
+            dist_x, dist_y = (gpx - REF_X, gpy - REF_Y)
             
             angle = math.atan2(dist_x, dist_y)
             
