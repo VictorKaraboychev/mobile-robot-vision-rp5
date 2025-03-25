ae460da5e2d482526b13904d319392c26139bd91
Sidharth Sreeram <sidharthsreeram@gmail.com>
2025-03-25 19:01:13 -0400

    changes

--- src/velocityVector.py
+++ src/velocityVector.py
@@ -21,6 +21,8 @@ Event = dict(
     Dropoff = int(4)
 )
 
+PickedUp = False
+
 # Camera intrinsic parameters
 focal_length_mm = 4.0
 diagonal_fov_deg = 55.0
@@ -119,8 +121,8 @@ def get_trajectory_vector(image):
     # Find contours in the mask
     contours_blue, _ = cv2.findContours(mask_blue, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
-    if contours_blue:
-        return True
+    # if contours_blue:
+    #     return True
     if contours_red:
         # Find the largest contour (assumed to be the red path)
         largest_contour = max(contours_red, key=cv2.contourArea)
