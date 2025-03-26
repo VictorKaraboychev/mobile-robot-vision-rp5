c7e7c0f30426746dcb4262f728240fba89467ce9
Sidharth Sreeram <sidharthsreeram@gmail.com>
2025-03-25 20:28:30 -0400

    changes

--- src/positionVector.py
+++ src/positionVector.py
@@ -44,7 +44,7 @@ K = np.array([[fx, 0, cx],
             [0, 0, 1]], dtype=np.float32)
 
 # Extrinsic parameters
-tilt_angle_deg = -35  # Tilt from horizontal
+tilt_angle_deg = -75  # Tilt from horizontal
 height = 0.0575  # 5 cm in meters
 
 # Rotation matrix (tilt around X-axis)
