d244e6ebf4b2ad58c630c33bf97735a84c4ac0d9
Sidharth Sreeram <sidharthsreeram@gmail.com>
2025-03-27 14:48:12 -0400

    heading

--- src/velocityVector.py
+++ src/velocityVector.py
@@ -215,7 +215,7 @@ def main():
             print("Arrived ")
             # input("enter to continue")
             i2c.write_block(0x05, [Event['Pickup']], '=B')
-            sleep(100)
+            sleep(0.100)
         elif trajectory:
             cx, cy, fx, fy = trajectory
             cp = find_real_world_coordinates(cx, cy)
